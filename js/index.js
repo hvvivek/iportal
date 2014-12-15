83441132d4d7f9eb83fc888912f67f7b42b2d8b3
sai <saikiranp321@gmail.com>
2014-12-15 15:29:43 +0530

    frontend changed

--- js/index.js
+++ js/index.js
@@ -2,43 +2,47 @@ $(document).ready(function(){
 	$("#one").hover(function()
 		{
 	//	$(this).delay(250).queue(function(next){$(this).css("background-color","lightblue");next();});
-		$(this).css("background-color","lightblue");
+		//$(this).css("background-color","lightblue");
 		$(".contain4").hide();
 		$(".contain3").hide();
 		$(".contain2").show();
-		$("#thr").css("background-color","lightgray");
-		$("#two").css("background-color","lightgray");
-		$("#four").css("background-color","lightgray")});
+		//$("#thr").css("background-color","lightgray");
+		//$("#two").css("background-color","lightgray");
+		//$("#four").css("background-color","lightgray");
+	});
 	$("#two").hover(function()
 		{
 	//	$(this).delay(250).queue(function(next){$(this).css("background-color","lightblue");next();});
-		$(this).css("background-color","lightblue");
+		//$(this).css("background-color","lightblue");
 		$(".contain2").hide();
 		$(".contain4").hide();
 		$(".contain3").show();
-		$("#one").css("background-color","lightgray");
-		$("#thr").css("background-color","lightgray");
-		$("#four").css("background-color","lightgray")});
+		//$("#one").css("background-color","lightgray");
+		//$("#thr").css("background-color","lightgray");
+		//$("#four").css("background-color","lightgray");
+	});
 	$("#thr").hover(function()
 		{
 	//	$(this).delay(250).queue(function(next){$(this).css("background-color","lightblue");next();});
-		$(this).css("background-color","lightblue");
+		//$(this).css("background-color","lightblue");
 		$(".contain2").hide();
 		$(".contain3").hide();
 		$(".contain4").show();
-		$("#two").css("background-color","lightgray");
-		$("#one").css("background-color","lightgray");
-		$("#four").css("background-color","lightgray")});
+		//$("#two").css("background-color","lightgray");
+		//$("#one").css("background-color","lightgray");
+		//$("#four").css("background-color","lightgray");
+	});
 	$("#four").hover(function()
 		{
 	//	$(this).delay(250).queue(function(next){$(this).css("background-color","lightblue");next();});
-		$(this).css("background-color","lightblue");
+		//$(this).css("background-color","lightblue");
 		$(".contain2").hide();
 		$(".contain3").hide();
 		$(".contain4").hide();
-		$("#two").css("background-color","lightgray");
-		$("#one").css("background-color","lightgray");
-		$("#thr").css("background-color","lightgray")});
+		//$("#two").css("background-color","lightgray");
+		//$("#one").css("background-color","lightgray");
+		//$("#thr").css("background-color","lightgray");
+	});
 	/*$("#thr").mouseout(function()
 	{
     		$("#thr").css("background-color","lightgray")});
