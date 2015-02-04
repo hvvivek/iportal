79575a27c69ea2d74da8535719c604d12d09251c
srikarrohit <srikarrohit@gmail.com>
2015-02-04 18:22:49 +0530

    cupschecked

--- hostel/hostel.php
+++ hostel/hostel.php
@@ -2,7 +2,7 @@
 session_start();
 $server="localhost";
 $user="root";
-$pwd="sai";
+$pwd="ragasree";
 $db="i-portal";
 $conn = mysql_connect($server,$user,$pwd);
 mysql_select_db("i-portal");
@@ -19,7 +19,7 @@ $lng=$row["longitude"];
 require '../includes/signin.php';
 ?>
 <html>
-<head>
+	<head>
 		<meta chaset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title><?php echo $name;?> hostel</title>
@@ -40,193 +40,212 @@ require '../includes/signin.php';
 		<script src="js/dynamicpage.js" type="text/javascript"></script>
 		<script type='text/javascript' src='js/jquery.ba-hashchange.min.js'></script>
 		<script src="../js/ferro.js" type="text/javascript"></script>
-<style>
-            #dnb_sec {
-                -skrollr-animation-name:animation1;
-            }
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<script src="js/jquery.js"></script>
+		<script src="js/bootstrap.js"></script>
+		<script src="http://maps.googleapis.com/maps/api/js"></script>	
+		<script src="../js/ferro.js" type="text/javascript"></script>
+		<script src="js/dynamicpage.js" type="text/javascript"></script>
+		<style>
+	            #dnb_sec {
+	                -skrollr-animation-name:animation1;
+	            }
 
-            @-skrollr-keyframes animation1 {
-                0 {
-                    margin-top:0px;
-                    position:relative;
-                    
-                    <!--Site Color -->
-                    background-color:rgba(26, 188, 156,1.0);
-                    <!--Site Color-->
-                }
+	            @-skrollr-keyframes animation1 {
+	                0 {
+	                    margin-top:0px;
+	                    position:relative;
+	                    
+	                    <!--Site Color -->
+	                    background-color:rgba(26, 188, 156,1.0);
+	                    <!--Site Color-->
+	                }
 
-                top {
-                    margin-top:0px;
-                    position:fixed;
-                    background-color:rgba(255, 255, 255,0.99);
-                }
-            }
-</style>
-<meta chaset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title><?php echo $name;?> hostel</title>
-<link href="css/bootstrap.min.css" rel="stylesheet">
-<link href="css/hostel.css" rel="stylesheet">
-<link href='http://fonts.googleapis.com/css?family=Lato:300,400' rel='stylesheet' type='text/css'/>
-<link rel="stylesheet" type="text/css" href="../css/ferro.css">
-<script src="js/jquery.js"></script>
-<script src="js/bootstrap.js"></script>
-<script src="http://maps.googleapis.com/maps/api/js"></script>				
-<script src="../js/ferro.js" type="text/javascript"></script>
-<script src="js/dynamicpage.js" type="text/javascript"></script>
-<script type='text/javascript' src='js/jquery.ba-hashchange.min.js'></script>
-<script type="text/javascript" src="../js/jquery.wheelmenu.js"></script>
-<link rel="stylesheet" type="text/css" href="../css/wheelmenu.css" />
-<script>
-		var lat='<?php echo $lat;?>';
-		var lng='<?php echo $lng;?>';
-		var myCenter=new google.maps.LatLng(lat,lng);
-		var myCenter=new google.maps.LatLng(lat,lng);
-		var map;
-			function initialize()
-			{
-				var mapProp = {
-			center:myCenter,
-			zoom:15,
-			mapTypeId:google.maps.MapTypeId.ROADMAP
-			};
-
-			map=new google.maps.Map(document.getElementById("googleMap1"),mapProp);
-
-			var marker=new google.maps.Marker({
-				position:myCenter,
-			});		
-			google.maps.event.addListenerOnce(map, 'idle', function() {
-				google.maps.event.trigger(map, 'resize');
-				map.setCenter(myCenter);
-			});
-			marker.setMap(map);
-			var infowindow = new google.maps.InfoWindow({
-			content: <?php echo $name ;?>
-			});
-			infowindow.open(map,marker);
-			}
+	                top {
+	                    margin-top:0px;
+	                    position:fixed;
+	                    background-color:rgba(305, 305, 305,0.99);
+	                }
+	            }
+		</style>
+		<script>
 			function access()
 			{
 				$('#Modal0').modal('show');
 				initialize();
-				/*setTimeout(function(){
-					
-					google.maps.event.trigger(map, "resize");
-					map.setCenter(myCenter);
-					},3000);*/
 			}		
-			//google.maps.event.addDomListener(window, 'load', initialize);
-			function wec(){
-			window.location= "../index.php";
-				}
-	</script>
-<script>
-		$(document).ready(function(){
-			$(".wheel-button").wheelmenu({
-        trigger: "hover",
-        animation: "fly",
-        animationSpeed: "fast"
+			function wec()
+			{
+				window.location= "../index.php";
+			}
+		</script>
+		<script>
+			$(document).ready(function()
+			{
+				$(".wheel-button").wheelmenu(
+				{
+		        	trigger: "hover",
+		        	animation: "fly",
+		        	animationSpeed: "fast"
 				});
-		});
-		
-</script>
-</head>
+			});
+				
+		</script>
+	</head>
 <body>
-        <?php
+	    <?php
 			require '../includes/navbar.php'
-        ?>
-<div class="container-fluid">
+	    ?>
+		<div class="container-fluid">
 			<?php
 				require 'sidebar.php'
-	        ?>
-		<div class="col-md-10 col-md-offset-2 col-sm-offset-2 col-sm-10">	
-					<?php
-						$sno=1;
-						while($sno<=8)
+			?>
+			<div class="col-md-10 col-md-offset-2 col-sm-offset-2 col-sm-10">	
+				<?php
+					$sno=1;
+					while($sno<=8)
+					{
+						$query="SELECT * FROM contacts WHERE hostel_id='{$id}' and s_no='{$sno}'";
+						$result=mysql_query($query);
+						$sec=mysql_fetch_assoc($result);
+						if($sno==4)
 						{
-							$query="SELECT * FROM contacts WHERE hostel_id='{$id}' and s_no='{$sno}'";
-							$result=mysql_query($query);
-							$sec=mysql_fetch_assoc($result);
-							if($sno==1||$sno==4||$sno==7)
-							{echo "
-									<div class='col-md-4 col-sm-6 screen screen12 slum'>
-										<div class='header'>
-											<p class='oned'>".$sec['post']."</p>
-										</div>
-										<div class='slider'>
-											<div id='s1'>
-												<div class='header1'>
-													<img class='img-circle img-responsive hidden-xs hidden-sm' src='img/photo.png'>
+							echo "
+								<div class='col-md-4 col-sm-6 screen screen12 slum'>
+									<div class='header'>
+										<p class='oned padaffairs'>";
+							echo nl2br($sec['post']."\nSecretary");
+							echo "</p>
+									</div>
+									<div class='slider'>
+										<div id='s1'>
+											<div class='header1'>
+												<img class='img-circle img-responsive hidden-xs hidden-sm' src='img/photo.png'>
 													<p class='hw'>".ucwords(strtolower(str_replace("."," ",$sec["name"]))).
 													"</p>
-												</div>
-											</div>
-											<div class='header2'>
-												<div class='heal' id='h1' class='hid'>
-													<p>";
-														echo "Roll no : ".$sec["username"];
-													echo "</p>
-												<p>";
-													echo "Room no : ".$sec["room"];
-												echo "</p>
-												<p>";
-													echo "Contact no : ".$sec["contact_no"];
-												echo "</p>
-												<p>";
-													echo "Email id : ".str_split($sec["email_id"],25)[0];
-												echo 
-												"</p>
-												</div>
 											</div>
 										</div>
-									</div>";
-								}
-								else
-								{
-									echo "
-									<div class='col-md-4 col-sm-6 screen slum'>
-										<div class='header'>
-											<p class='oned'>".$sec['post']."</p>
-										</div>
-										<div class='slider'>
-											<div id='s1'>
-												<div class='header1'>
-													<img class='img-circle img-responsive hidden-xs hidden-sm' src='img/photo.png'>
-													<p class='hw'>".ucwords(strtolower(str_replace("."," ",$sec["name"]))).
-													"</p>
-												</div>
-											</div>
-											<div class='header2'>
-												<div class='heal' id='h1' class='hid'>
-													<p>";
-													echo "Roll no : ".$sec["username"];
-													echo "</p>
+										<div class='header2'>
+											<div class='heal' id='h1' class='hid'>
 												<p>";
-													echo "Room no : ".$sec["room"];
+												echo "Roll no : ".$sec["username"];
+												echo "</p><p>";
+												echo "Room no : ".$sec["room"];
+												echo "</p><p>";
+												echo "Contact no : ".$sec["contact_no"];
+												echo "</p><p>";
+												echo "Email id : ".str_split($sec["email_id"],30)[0];
 												echo "</p>
-												<p>";
-													echo "Contact no : ".$sec["contact_no"];
-												echo "</p>
-												<p>";
-													echo "Email id : ".$sec["email_id"];
-												echo 
-												"</p>
-												</div>
 											</div>
 										</div>
-									</div>";
-								}
-							$sno++;
+									</div>
+								</div>";
 						}
-					?>
-	</div>
-</div>
-<script type="text/javascript" src="../js/skrollr.stylesheets.js"></script>
-<script type="text/javascript" src="../js/skrollr.js"></script>
-<script type="text/javascript">skrollr.init();</script>
-<?php
-	require '../partials/footer.php';
-?>
-</body>
+						else if($sno==1||$sno==7)
+						{
+							echo "
+								<div class='col-md-4 col-sm-6 screen screen12 slum'>
+									<div class='header'>
+										<p class='oned padnone'>".$sec['post']."</p>
+									</div>
+									<div class='slider'>
+										<div id='s1'>
+											<div class='header1'>
+												<img class='img-circle img-responsive hidden-xs hidden-sm' src='img/photo.png'>
+													<p class='hw'>".ucwords(strtolower(str_replace("."," ",$sec["name"]))).
+													"</p>
+											</div>
+										</div>
+										<div class='header2'>
+											<div class='heal' id='h1' class='hid'>
+												<p>";
+												echo "Roll no : ".$sec["username"];
+												echo "</p><p>";
+												echo "Room no : ".$sec["room"];
+												echo "</p><p>";
+												echo "Contact no : ".$sec["contact_no"];
+												echo "</p><p>";
+												echo "Email id : ".str_split($sec["email_id"],30)[0];
+												echo "</p>
+											</div>
+										</div>
+									</div>
+								</div>";
+						}
+						else if($sno==5||$sno==6||$sno==8)
+						{
+							echo "
+								<div class='col-md-4 col-sm-6 screen slum'>
+									<div class='header'>
+										<p class='oned padaffairs'>";
+							echo nl2br($sec['post']."\nSecretary");
+							echo "</p>
+								</div>
+									<div class='slider'>
+										<div id='s1'>
+											<div class='header1'>
+												<img class='img-circle img-responsive hidden-xs hidden-sm' src='img/photo.png'>
+													<p class='hw'>".ucwords(strtolower(str_replace("."," ",$sec["name"]))).
+													"</p>
+											</div>
+										</div>
+										<div class='header2'>
+											<div class='heal' id='h1' class='hid'>
+												<p>";
+												echo "Roll no : ".$sec["username"];
+												echo "</p><p>";
+												echo "Room no : ".$sec["room"];
+												echo "</p><p>";
+												echo "Contact no : ".$sec["contact_no"];
+												echo "</p><p>";
+												echo "Email id : ".str_split($sec["email_id"],30)[0];
+												echo "</p>
+											</div>
+										</div>
+									</div>
+								</div>";
+						}
+						else
+						{
+							echo "
+								<div class='col-md-4 col-sm-6 screen slum'>
+									<div class='header'>
+										<p class='oned padnone'>".$sec['post']."</p>
+									</div>
+									<div class='slider'>
+										<div id='s1'>
+											<div class='header1'>
+												<img class='img-circle img-responsive hidden-xs hidden-sm' src='img/photo.png'>
+													<p class='hw'>".ucwords(strtolower(str_replace("."," ",$sec["name"]))).
+													"</p>
+											</div>
+										</div>
+										<div class='header2'>
+											<div class='heal' id='h1' class='hid'>
+												<p>";
+												echo "Roll no : ".$sec["username"];
+												echo "</p><p>";
+												echo "Room no : ".$sec["room"];
+												echo "</p><p>";
+												echo "Contact no : ".$sec["contact_no"];
+												echo "</p><p>";
+												echo "Email id : ".str_split($sec["email_id"],30)[0];
+												echo "</p>
+											</div>
+										</div>
+									</div>
+								</div>";
+						}
+						$sno++;
+					}
+				?>
+			</div>
+		</div>
+		<script type="text/javascript" src="../js/skrollr.stylesheets.js"></script>
+		<script type="text/javascript" src="../js/skrollr.js"></script>
+		<script type="text/javascript">skrollr.init();</script>
+		<?php
+			require '../partials/footer.php';
+		?>
+	</body>
 </html>
