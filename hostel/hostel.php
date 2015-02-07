7cbc47177b121c91d7b2f15dfd10b4d028ae736e
srikarrohit <srikarrohit@gmail.com>
2015-02-08 01:14:56 +0530

    indexpagejscorrected

--- hostel/hostel.php
+++ hostel/hostel.php
@@ -147,9 +147,19 @@ $lng=$row["long"];
 			require '../includes/navbar.php'
         ?>
 		<div class="container-fluid">
-		<?php
-			require 'sidebar.php'
-        ?>
+		<div class="sidebar col-md-2 col-sm-2">
+	<ul class="nav nav-sidebar">
+		<li class="bull bull1"><a href='#' id="hos"><?php echo $name;?> Hostel</a></li>
+		<li class="bull active"><a href='#' class="kill actived">Hostel secretaries details</a></li>
+		<li class="bull"><?php echo "<a href='office_details.php?varname=$id'";?> class="kill">Hostel office-details</a></li>
+		<li class="bull"><a href='hostel_services.php' class="kill">Hostel services</a></li>
+		<li class="bull"><a href='../event.php?hostel=<?php echo $_GET['varname']?>&sec=litsoc' class="kill">Litsoc</a></li>
+		<li class="bull"><a href='../event.php?hostel=<?php echo $_GET['varname']?>&sec=techsoc' class="kill">Techsoc</a></li>
+		<li class="bull"><a href='../event.php?hostel=<?php echo $_GET['varname']?>&sec=sports' class="kill">Schroeter</a></li>
+		<li class="bull"><a href='../event.php?hostel=<?php echo $_GET['varname']?>&sec=alumni' class="kill">Alumni</a></li>
+		<li class="bull"><a href="#" class="kill" onclick="access();">Location</a></li> 
+	</ul>
+</div>
 			<div class="col-md-10 col-md-offset-2 col-sm-offset-2 col-sm-10">	
 				<?php
 					$sno=1;
